1ab8c783ea1c8ca8623ef04c4d4e9f692725507c
Al4nX <alank-po@outlook.es>
2023-06-28 20:04:57 -0300

    arreglando errores de CORS

--- src/main/java/com/portafolio/portafolio/Controller/CExperiencia.java
+++ src/main/java/com/portafolio/portafolio/Controller/CExperiencia.java
@@ -29,7 +29,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfolio-frontend-68aac.web.app")
 public class CExperiencia {
 
     @Autowired
